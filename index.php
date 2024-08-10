0d796938f77462b2e22eebe793970e3737746bd7
codepipe <47555726+codepipe@users.noreply.github.com>
2024-08-10 22:37:38 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Hi Ranjitha, Eshwari, Milana, Harsha , Phani , Satvik Hemanth</h1>
+
+<?php
+echo "Welcome to the PAAS in AWS cloud , Hello World!";
+?>
+
+</body>
+</html>
